7d9f6aa17a77e67e121da825a6281c0b53e3dd5d
Igor Usenko <usenkoigor01@gmail.com>
2025-04-05 14:38:37 +0300

    Добавлены новые сервисы и исправлена проблема с показом оповещения об обновлении

--- popup.js
+++ popup.js
@@ -90,7 +90,7 @@ document.addEventListener("DOMContentLoaded", function () {
     "https://huggingface.co/spaces/Lightricks/LTX-Video-Playground","https://huggingface.co/spaces/LGAI-EXAONE/EXAONE-3.5-7.8B-Instruct-Demo","https://huggingface.co/spaces/webml-community/text-to-speech-webgpu","https://t.me/gpt_lama_bot","https://www.hailuo.ai/","https://v0.dev/","https://www.whatmore.ai/studio","https://huggingface.co/spaces/tencent/Hunyuan3D-2","https://www.dzine.ai/",
     "https://huggingface.co/spaces/deepseek-ai/deepseek-vl2-small","https://huggingface.co/spaces/Trudy/gemini-image-to-code","https://chat.deepseek.com/","https://backgrounderase.net/home","https://huggingface.co/spaces/webml-community/kokoro-webgpu","https://www.morphic.sh/","https://scira.app/","https://kagi.com/fastgpt","https://shapen.com/","https://alice.yandex.ru/chat/01953c1a-be79-4000-9e88-5177131e2739/",
     "https://notedly.ai/dashboard","https://playground.ai.cloudflare.com/","https://huggingface.co/spaces/ASLP-lab/DiffRhythm","https://chat.akash.network/","https://www.sesame.com/research/crossing_the_uncanny_valley_of_voice#demo","https://www.imagineanything.ai/","https://huggingface.co/spaces/prs-eth/thera","https://www.openai.fm/","https://huggingface.co/spaces/starvector/starvector-1b-im2svg",
-    "https://huggingface.co/spaces/enzostvs/deepsite"
+    "https://huggingface.co/spaces/enzostvs/deepsite","https://huggingface.co/spaces/jasperai/Flux.1-dev-Controlnet-Upscaler","https://huggingface.co/spaces/nightfury/Image_Face_Upscale_Restoration-GFPGAN","https://auphonic.com/"
   ];
 
 
@@ -1173,7 +1173,11 @@ canOpen.nextSibling.textContent = translateText("Скрыть сервисы, к
       "https://huggingface.co/spaces/starvector/starvector-1b-im2svg":"Нейросеть для генерации SVG из любой картинки",
       "https://noowai.com/":"Бесплатный чат-бот и помощник, который может бесплатно общаться, отвечать на вопросы и помогать вам с работой",
       "https://chat.typegpt.net/":"Сервис предоставляет доступ к различным LLM, работает не во всех браузерах",
-      "https://huggingface.co/spaces/enzostvs/deepsite":"Бесплатный аналог Cursor прямо в браузере"
+      "https://huggingface.co/spaces/enzostvs/deepsite":"Бесплатный аналог Cursor прямо в браузере",
+      "https://auphonic.com/":"Бесплатная нейросеть, которая буквально спасает испорченные записи",
+      "https://huggingface.co/spaces/jasperai/Flux.1-dev-Controlnet-Upscaler":"Сервис для повышения качества изображений",
+      "https://huggingface.co/spaces/nightfury/Image_Face_Upscale_Restoration-GFPGAN":"Сервис для повышения качества изображений",
+      "https://www.clear-background.com/remove-bg":"Сервис позволяет удалить фон у изображений"
   };    
 
 function applyTheme(backgroundColor, textColor, liColor, liTextColor) { 
